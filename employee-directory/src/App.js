43946e61dbdb9791a735d0cb78444d117ecf5912
Nathan Gianelli <nathan.gianelli98@gmail.com>
2020-03-31 22:14:15 -0400

    started nav bar and container

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Nav from "./components/nav"
+import Container from "./components/container"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Container>
+    
+    <Nav/>
+
+    </Container>
   );
 }
 
---
+++ employee-directory/src/components/nav.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <div className = "navbar">
+            123
+                <h1>Employee Directory</h1>
+            </div>
+        )
+    }
+}
+
+export default Nav;
